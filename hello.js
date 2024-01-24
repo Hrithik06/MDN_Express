644e488439b3e1bdbbac6515057c742aa7354023
Hrithik06 <el99el03@gmail.com>
2024-01-24 17:30:08 +0530

    basic routing to wiki page

--- hello.js
+++ hello.js
@@ -1,7 +1,10 @@
 import express from "express";
 import wiki from "./wiki.js";
+import logger from "morgan"
 const app = express();
 const port = 3000;
+
+app.use(logger("dev"));
 app.use("/wiki", wiki)
 app.get("/", function (req, res) {
   res.send("Hello World!");
